ef5803acff12773d7337b7f68a7d93c173aca112
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-22 11:30:31 -0700

    e
    e

---
+++ src/_03_smurf/Smurf_Runner.java
@@ -0,0 +1,12 @@
+package _03_smurf;
+
+public class Smurf_Runner {
+public static void main(String[] args) {
+	new Smurf("Handy Smurf").getName();
+	new Smurf("Handy Smurf").eat();
+	new Smurf("Papa Smurf").getName();
+	new Smurf("Papa Smurf").getHatColor();
+	new Smurf("Smurfette").getName();
+	new Smurf("Smurfette").isGirlOrBoy();
+}
+}
