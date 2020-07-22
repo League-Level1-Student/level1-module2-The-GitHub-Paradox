f291240af62603a1d38d875a1df84306a8e370ba
Meme1sh <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-07-22 11:11:30 -0700

    e
    e

---
+++ src/_02_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,12 @@
+package _02_sea_creature;
+
+public class SeaCreatureRunner {
+	public static void main(String[] args) {
+		new SeaCreature("Spongebob").eat();
+		new SeaCreature("Spongebob").laugh();
+		new SeaCreature("Patrick").eat();
+		new SeaCreature("Patrick").laugh();
+		new SeaCreature("Squidward").eat();
+		new SeaCreature("Squidward").laugh();
+	}
+}
